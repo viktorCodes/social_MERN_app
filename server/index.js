9b061b9cb42296406e13596f8b547491b73881a9
calimendoza <mbaivictor1@gmail.com>
2023-01-23 06:55:12 -0800

    restructured app folder separate client and server

---
+++ server/index.js
@@ -0,0 +1,72 @@
+import express from  "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { fileURLToPath } from "url";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
+
+//CONFIGURATIONS
+
+const __filname = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filname);
+dotenv.config();
+const app = express();
+ app.use(express.json());
+ app.use(helmet());
+ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+ app.use(morgan("common"));
+ app.use(cors());
+ app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+ app.use(express.urlencoded({extended: true}));
+
+
+ //FILE STORAGE
+
+ const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "public/assets");
+    },
+
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
+ })
+const upload = multer({storage});
+
+//ROUTES WITH FILE
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost );
+
+/* ROUTES */
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+//MONGOOSE SETUP
+
+mongoose.set('strictQuery', false);
+
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT,  () => console.log(`Amandla, Awethu... Listen on PORT ${PORT}`));
+         /** ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+}).catch((error) => console.log(` ERROR on ${error} : Check It `))
