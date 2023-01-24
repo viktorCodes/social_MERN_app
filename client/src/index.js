ea80f23af47995601f4b8431d501114c8cd121e0
calimendoza <mbaivictor1@gmail.com>
2023-01-24 13:23:34 -0800

    finished state management with redux toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,53 @@ import ReactDOM from 'react-dom/client';
 import "./index.css";
 import authReducer from "./state";
 import App from './App';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
 
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PURGE,
+  PERSIST,
+  REGISTER
+} from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from 'redux-persist/integration/react';
+
+const store = configureStore({
+  reducer: persistReducer, 
+  middleware: (getDefaultMiddleware) => 
+  getDefaultMiddleware({
+
+    serializeableCheck: {
+
+ignoredActions: [FLUSH, REHYDRATE, PAUSE, PURGE, PERSIST, REGISTER]
+
+    }
+  })
+  
+});
+
+   
+
+
+const persistConfig = { key: "root", storage, version: 1};
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistStore(store)}>
+
     <App />
+
+    </PersistGate>
+   </Provider>
   </React.StrictMode>
 );
 
