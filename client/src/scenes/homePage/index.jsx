06b31e0575fd7fd6ee4c22180b8fc31ff1bd8234
calimendoza <mbaivictor1@gmail.com>
2023-01-26 08:59:18 -0800

    finished navbar

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,18 @@
+import { Box } from "@mui/material";
+//import { useSelector } from "react-redux";
+import Navbar from "../navbar";
+
+
+
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+     <Box>
+
+       <Navbar />
+        </Box>
+
   )
+  
 };
 
 
