a10ce6650aff009059ea807f70f8abecc494ca92
calimendoza <mbaivictor1@gmail.com>
2023-01-27 02:22:13 -0800

    authorization and authentication complete

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,3 +1,6 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from './Form';
+
 export default function LoginPage() {
     return (
       <div>LoginPage</div>
